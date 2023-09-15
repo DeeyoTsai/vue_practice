147b9479199aad7aa4bee1c78a138d43454f5971
deeyo <deeyo0312@gmail.com>
2023-09-15 15:01:43 +0800

    add tailwind css and remove method

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,14 @@
 <script>
+import deviceInfo from './components/info.vue';
+import deviceItem from './components/item.vue';
+import { v4 as uuidv4 } from 'uuid';
+
 
 export default {
+  components:{
+    deviceInfo,
+    deviceItem
+  },
   data: function() {
     return {
       title:'AUO',
@@ -11,10 +19,21 @@ export default {
   methods:{
     addDevice() {
       if (this.device !== ""){
-        this.devices.unshift(this.device);
-        // console.log(this.device);
+        const item = {
+          id:uuidv4(),
+          title:this.device,
+        };
+        this.devices.unshift(item);
         this.device = "";
       };
+    },
+    removeDevice(id) {
+      // console.log(id);
+      const index = this.devices.findIndex((e) => {
+          return e.id === id;
+        });
+      // console.log(index);
+      this.devices.splice(index,1);
     },
   },
 };
@@ -22,24 +41,42 @@ export default {
 </script>
 
 <template>
-<h1> {{ title }} 設備管理系統 </h1>
-<form>
-  <label for="">設備名稱</label><br>
-  <input type="text" v-model="device">
-  <button>新增</button>
-</form>
-<section>
-  <h3 v-if="devices.length == 0">目前無任何設備</h3>
-  <h3 v-else>目前共計{{ devices.length }}項設備</h3>
+<main class="mx-6 sm:mx-16">
+  <h1 class="my-6 text-4xl font-extrabold"> {{ title }} 設備管理系統 </h1>
+  <form>
+    <label for="">設備名稱</label><br>
+    <div class="flex gap-2 items-center">
+      <input
+            type="text"
+            placeholder="請輸入設備名稱"
+            class="input-device"
+            v-model="device"
+      />
+      <button @click.prevent="addDevice" class="btn btn-action">新增</button>
+    </div>
+  </form>
+  <section>
 
-</section>
+    <deviceInfo v-bind:devices="devices" />
+
+    <deviceItem @remove-auo-item="removeDevice" 
+                v-for="d in devices" 
+                :it="d" 
+    />
+
+  </section>
+
+</main>
 </template>
 
 <style scoped>
-label {
-  background-color: black;
-  color: white;
-  padding: 4px 8px;
+.input-device {
+  @apply my-2 px-2 py-2 text-xl border border-slate-500;
 }
-
+.btn {
+  @apply mx-1 py-2 px-4 select-none;
+}
+.btn-action {
+  @apply bg-black text-white hover:bg-slate-600;
+}
 </style>
---
+++ src/components/item.vue
@@ -0,0 +1,31 @@
+<script>
+export default {
+    props:{
+        it:{
+            type:Object,
+            require:true,
+        },
+    },
+    methods:{
+        remove(){
+            this.$emit('remove-auo-item', this.it.id);
+        },
+    },
+};
+</script>
+
+
+<template>
+
+<ul>
+    <li>{{ it.title }}</li>
+    <button @click="remove" 
+            class="bg-black text-white px-2 py-1"
+    > X </button>
+</ul>        
+    
+</template>
+
+<style scoped>
+
+</style>
